0a8ba5ed6f51fec9988dd7de0e546c4c4ebfe5ef
Julien <julienawilson@gmail.com>
2017-03-01 12:54:51 -0800

    registration views and templates

--- news_project/news_project/urls.py
+++ news_project/news_project/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from news_project.views import HomeView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^articles/', include("articles.urls"))
-
+    url(r'^articles/', include("articles.urls")),
+    url(r'^registration/', include("registration.backends.hmac.urls")),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 
 ]
