6fc8c70127266126aff1b3c2d362c790872f5d11
Julien <julienawilson@gmail.com>
2017-03-02 23:38:40 -0800

    add short and long descriptions for articles.  numbers on list

--- news_project/articles/models.py
+++ news_project/articles/models.py
@@ -6,6 +6,8 @@ class Article(models.Model):
     """Article moel for the article app."""
 
     title = models.CharField(max_length=255)
+    short_desc = models.CharField(max_length=255, default='', blank=True, null=True)
+    long_desc = models.CharField(max_length=1000, default='', blank=True, null=True)
     link = models.CharField(max_length=255)
     votes = models.IntegerField(default=1)
     date_submitted = models.DateTimeField(default=timezone.now)
