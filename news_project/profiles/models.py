7c6cf4be4bface199c8d05bdc7a23323a99459c8
Julien <julienawilson@gmail.com>
2017-03-05 09:45:11 -0800

    user profile model

--- news_project/profiles/models.py
+++ news_project/profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ActiveUsersManager(models.Manager):
+    """Active user manager."""
+
+    def get_queryset(self):
+        """Get the full query of active users."""
+        return super(ActiveUsersManager, self).get_queryset().filter(user__is_active=True)
+
+
+class UserProfile(models.Model):
+    """Profile for the user."""
+
+    objects = models.Manager()
+    active = ActiveUsersManager()
+
+    user = models.OneToOneFiels(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    
