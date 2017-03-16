50c72b8e6581bf342d1281e4bf1773dceb80108c
Julien <julienawilson@gmail.com>
2017-03-16 07:30:58 -0700

    fixed broken link to posting

--- news_project/articles/urls.py
+++ news_project/articles/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^post/$', PostArticleView.as_view(), name='post_article'),
     url(r'^(?P<pk>\d+)/$', ArticleDetail.as_view(), name='article_detail'),
     url(r'^(?P<tag>\w+)/$', TagView.as_view(), name='tag_list'),
-    url(r'^api/list/$', ArticleListAPIView.as_view(), name='post_article'),
-    url(r'^api/(?P<pk>\d+)/$', ArticleDetailAPIView.as_view(), name='article_detail'),
+    url(r'^api/list/$', ArticleListAPIView.as_view(), name='api_list'),
+    url(r'^api/(?P<pk>\d+)/$', ArticleDetailAPIView.as_view(), name='api_detail'),
 ]
